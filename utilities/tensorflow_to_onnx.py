6139bffc5d89198d7e4a1f97e0cfbd52a7afcecc
hayden-donnelly <hayden.git@proton.me>
2023-01-30 17:32:38 -0500

    Image to image experiment.

--- utilities/tensorflow_to_onnx.py
+++ utilities/tensorflow_to_onnx.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import tf2onnx
 
 input_path = "../data/models/diffusion_models/"
-model_name = "diffusion1_epoch_36"
+model_name = "diffusion1_epoch62"
 file_type = ''
 output_path = "../data/models/onnx_models/"
 
@@ -10,4 +10,4 @@ output_path = "../data/models/onnx_models/"
 pre_model = tf.keras.models.load_model(input_path + model_name + file_type)
 
 # Convert h5 to onnx.
-tf2onnx.convert.from_keras(pre_model, output_path = output_path + model_name + ".onnx")
+tf2onnx.convert.from_keras(pre_model, output_path = output_path + model_name + ".onnx", opset = 9)
