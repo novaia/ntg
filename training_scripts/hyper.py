f2461f5c8962cbb1ba81ab11de026753d5f65284
hayden-donnelly <donnellyhd@outlook.com>
2023-02-22 22:39:32 -0500

    Renamed training scripts and added some training code for hyper.

---
+++ training_scripts/hyper.py
@@ -0,0 +1,327 @@
+"""
+Training code for the latent diffusion model Hyper.
+"""
+
+import tensorflow as tf
+from keras import layers
+import keras
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+from keras.preprocessing.image import ImageDataGenerator
+import os
+import random
+
+# Data.
+image_size = 128
+channels = 3
+color_mode = 'rgb'
+image_dataset_path = '../lsun_bedroom_sample/'
+latent_dataset_path = '../lsun_bedroom_latent/'
+
+# Autoencoder.
+dropout = 0.1
+autoencoder_batch_size = 128
+autoencoder_epochs = 10
+autoencoder_widths = [32, 64, 96]
+autoencoder_block_depth = 3
+latent_size = int(image_size / 2**len(autoencoder_widths))
+beta_slope = 0.0002
+beta_period = 10000
+max_beta = 1.0
+min_beta = 0.0
+
+# Diffusion.
+diffusion_batch_size = 1024
+diffusion_epochs = 100
+min_signal_rate = 0.02
+max_signal_rate = 0.95
+embedding_dims = 32
+embedding_max_frequency = 1000.0
+diffusion_widths = [32, 64, 96]
+diffusion_block_depth = 2
+
+class LatentGenerator(tf.keras.utils.Sequence):
+    def __init__(self, path, batch_size, shuffle, target_size, channels):
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.path = path
+        self.file_list = os.listdir(path)
+        self.num_files = len(self.file_list)
+        self.target_size = target_size
+        self.channels = channels
+        
+        print('Found ' + str(self.num_files) + ' files.')
+    
+    def on_epoch_end(self):
+        if self.shuffle:
+            self.file_list = random.sample(self.file_list, self.num_files)
+    
+    def __len__(self):
+        return self.num_files // self.batch_size
+    
+    def __getitem__(self, index):
+        arr = np.zeros(shape=(self.batch_size, self.target_size, self.target_size, channels))
+        for i in range(self.batch_size):
+            arr[i] = np.load(self.path + self.file_list[index + i])        
+        return arr
+    
+class BetaScheduler(keras.callbacks.Callback):
+    def on_train_batch_end(self, batch, logs=None):
+        if(self.model.beta_schedule_step == self.model.beta_period):
+            self.model.beta.assign(min_beta)
+            self.model.beta_schedule_step = 0
+        if self.model.beta < max_beta:
+            self.model.beta.assign(self.model.beta_slope * self.model.beta_schedule_step)
+        if self.model.beta >= max_beta:
+            self.model.beta.assign(max_beta)
+        
+        self.model.beta_history.append(self.model.beta.numpy())
+        self.model.beta_schedule_step += 1
+        return
+    
+class Sampling(layers.Layer):
+    def call(self, inputs):
+        z_mean, z_log_var = inputs
+        epsilon = tf.keras.backend.random_normal(shape=(z_mean.shape[1], z_mean.shape[2], channels))
+        return z_mean + tf.exp(0.5 * z_log_var) * epsilon
+
+class VAE(keras.Model):
+    def __init__(self, widths, beta_slope, beta_period, encoder=None, decoder=None):
+        super().__init__()
+        self.encoder_widths = widths
+        self.decoder_widths = list(reversed(widths))
+        
+        if encoder != None:
+            self.encoder = encoder
+        else:
+            self.encoder = self.create_encoder()
+            
+        if decoder != None:
+            self.decoder = decoder
+        else:
+            self.decoder = self.create_decoder()
+        
+        self.total_loss_tracker = keras.metrics.Mean(name="total_loss")
+        self.reconstruction_loss_tracker = keras.metrics.Mean(
+            name="reconstruction_loss"
+        )
+        self.kl_loss_tracker = keras.metrics.Mean(name="kl_loss")
+        
+        self.beta_schedule_step = 0
+        self.beta = tf.Variable(0.0, trainable=False)
+        self.beta_slope = beta_slope
+        self.beta_period = beta_period
+        self.beta_history = []
+    
+    @property
+    def metrics(self):
+        return [
+            self.total_loss_tracker,
+            self.reconstruction_loss_tracker,
+            self.kl_loss_tracker,
+        ]
+    
+    def create_encoder(self):
+        input_image = layers.Input(shape=(image_size, image_size, channels))
+        x = input_image
+
+        for width in self.encoder_widths:
+            if x.shape[0] == width:
+                residual = x
+            else:
+                residual = layers.Conv2D(width, (1, 1))(x)
+            
+            for _ in range(autoencoder_block_depth):
+                x = layers.Conv2D(width, (3, 3), activation='relu', padding='same')(x)
+            
+            x = layers.Add()([x, residual])
+            x = layers.LayerNormalization()(x)
+            x = layers.MaxPooling2D((2, 2), padding='same')(x)
+
+        z_mean = layers.Conv2D(channels, (2, 2), padding='same')(x)
+        z_log_var = layers.Conv2D(channels, (2, 2), padding='same')(x)
+        z = Sampling()([z_mean, z_log_var])
+        return keras.Model(input_image, [z_mean, z_log_var, z])
+    
+    def create_decoder(self):
+        input_latent = layers.Input(shape=(latent_size, latent_size, channels))
+        x = input_latent
+
+        for width in self.decoder_widths:
+            if x.shape[0] == width:
+                residual = x
+            else:
+                residual = layers.Conv2D(width, (1, 1))(x)
+            for _ in range(autoencoder_block_depth):
+                x = layers.Conv2D(width, (3, 3), activation='relu', padding='same')(x)
+            x = layers.Add()([x, residual])
+            x = layers.LayerNormalization()(x)
+            x = layers.UpSampling2D((2, 2))(x)
+            
+        x = layers.Conv2D(channels, (3, 3), activation='sigmoid', padding='same')(x)
+
+        return keras.Model(input_latent, x)
+    
+    def train_step(self, data):
+        data = data[0] # Get rid of labels.
+        
+        with tf.GradientTape() as tape:
+            # Encoding/Decoding.
+            z_mean, z_log_var, z = self.encoder(data)
+            reconstruction = self.decoder(z)
+            
+            # loss.
+            reconstruction_loss = tf.reduce_mean(
+                tf.reduce_sum(
+                    keras.losses.mean_squared_error(data, reconstruction), axis=(1, 2)
+                )
+            )
+                
+            kl_loss = -self.beta * (1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var))
+            kl_loss = tf.reduce_mean(tf.reduce_sum(kl_loss, axis=1))
+            total_loss = reconstruction_loss + kl_loss
+            
+        grads = tape.gradient(total_loss, self.trainable_weights)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
+
+        self.total_loss_tracker.update_state(total_loss)
+        self.reconstruction_loss_tracker.update_state(reconstruction_loss)
+        self.kl_loss_tracker.update_state(kl_loss)
+        
+        return {
+            "loss": self.total_loss_tracker.result(),
+            "reconstruction_loss": self.reconstruction_loss_tracker.result(),
+            "kl_loss": self.kl_loss_tracker.result(),
+        }
+    
+    def encode(self, data):
+        _, _, z = self.encoder(data, training=False)
+        return z
+    
+    def decode(self, data):
+        x = self.decoder(data, training=False)
+        return x
+
+class DiffusionModel(keras.Model):
+    def __init__(self, input_size, block_depth, widths):
+        super().__init__()
+        
+        self.normalizer = layers.Normalization()
+        self.model = self.create_model(input_size, widths, block_depth)
+        self.input_size = input_size
+    
+    def create_model(self, input_size, widths, block_depth):
+        noisy_input = keras.Input(shape=(input_size, input_size, channels))
+        noise_variances = keras.Input(shape=(1, 1, 1))
+        
+        e = layers.Lambda(sinusoidal_embedding)(noise_variances)
+        e = layers.UpSampling2D(size=input_size, interpolation='nearest')(e)
+        
+        x = layers.Conv2D(widths[0], kernel_size=1)(noisy_input)
+        x = layers.Concatenate()([x, e])
+        
+        skips = []
+        for width in widths[:-1]:
+            x = DownBlock(width, block_depth)([x, skips])
+            
+        for _ in range(block_depth):
+            x = ResidualBlock(widths[-1])(x)
+        
+        for width in reversed(widths[:-1]):
+            x = UpBlock(width, block_depth)([x, skips])
+            
+        x = layers.Conv2D(channels, kernel_size=1, kernel_initializer='zeros')(x)
+        
+        return keras.Model([noisy_input, noise_variances], x)
+    
+    def compile(self, **kwargs):
+        super().compile(**kwargs)
+        
+        self.noise_loss_tracker = keras.metrics.Mean(name='n_loss')
+        self.image_loss_tracker = keras.metrics.Mean(name='i_loss')
+    
+    @property
+    def metrics(self):
+        return [self.noise_loss_tracker, self.image_loss_tracker]
+    
+    def diffusion_schedule(self, diffusion_times):
+        start_angle = tf.acos(max_signal_rate)
+        end_angle = tf.acos(min_signal_rate)
+
+        diffusion_angles = start_angle + diffusion_times * (end_angle - start_angle)
+
+        signal_rates = tf.cos(diffusion_angles)
+        noise_rates = tf.sin(diffusion_angles)
+
+        return noise_rates, signal_rates
+    
+    def denoise(self, noisy_images, noise_rates, signal_rates, training):
+        pred_noises = self.model([noisy_images, noise_rates**2], training=training)
+        pred_images = (noisy_images - noise_rates * pred_noises) / signal_rates
+        
+        return pred_noises, pred_images
+    
+    def reverse_diffusion(self, initial_noise, diffusion_steps):
+        num_images = initial_noise.shape[0]    
+        step_size = 1.0 / diffusion_steps
+        
+        next_noisy_images = initial_noise
+        for step in range(diffusion_steps):
+            noisy_images = next_noisy_images
+            
+            diffusion_times = tf.ones((num_images, 1, 1, 1)) - step * step_size
+            noise_rates, signal_rates = self.diffusion_schedule(diffusion_times)
+            pred_noises, pred_images = self.denoise(
+                noisy_images, noise_rates, signal_rates, training=False
+            )
+            
+            next_diffusion_times = diffusion_times - step_size
+            next_noise_rates, next_signal_rates = self.diffusion_schedule(
+                next_diffusion_times
+            )
+            next_noisy_images = (
+                next_signal_rates * pred_images + next_noise_rates * pred_noises
+            )
+            
+        return pred_images
+    
+    def generate(self, num_images, diffusion_steps):
+        initial_noise = tf.random.normal(shape=(num_images, self.input_size, self.input_size, channels))
+        generated_images = self.reverse_diffusion(initial_noise, diffusion_steps)
+        return generated_images
+    
+    def train_step(self, images):        
+        noises = tf.random.normal(shape=(diffusion_batch_size, self.input_size, self.input_size, channels))
+        
+        diffusion_times = tf.random.uniform(
+            shape=(diffusion_batch_size, 1, 1, 1), minval=0.0, maxval=1.0
+        )
+        
+        noise_rates, signal_rates = self.diffusion_schedule(diffusion_times)
+        noisy_images = signal_rates * images + noise_rates * noises
+        
+        with tf.GradientTape() as tape:
+            pred_noises, pred_images = self.denoise(
+                noisy_images, noise_rates, signal_rates, training=True
+            )
+            
+            noise_loss = tf.reduce_mean(
+                tf.reduce_sum(
+                    keras.losses.mean_squared_error(noises, pred_noises), axis=(1, 2)
+                )
+            )
+            
+            image_loss = tf.reduce_mean(
+                tf.reduce_sum(
+                    keras.losses.mean_squared_error(images, pred_images), axis=(1, 2)
+                )
+            )
+            
+        grads = tape.gradient(noise_loss, self.model.trainable_weights)
+        self.optimizer.apply_gradients(zip(grads, self.model.trainable_weights))
+
+        self.noise_loss_tracker.update_state(noise_loss)
+        self.image_loss_tracker.update_state(image_loss)
+            
+        return {m.name: m.result() for m in self.metrics}
