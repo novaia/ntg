31ccdffc7311a17a7d0afc1c15e0870c261e082f
hayden-donnelly <hayden.git@proton.me>
2023-02-26 22:07:26 -0500

    Added unet blocks.

--- training_scripts/hyper.py
+++ training_scripts/hyper.py
@@ -203,6 +203,61 @@ class VAE(keras.Model):
         x = self.decoder(data, training=False)
         return x
 
+def sinusoidal_embedding(x):
+    embedding_min_frequency = 1.0
+    frequencies = tf.exp(
+        tf.linspace(
+            tf.math.log(embedding_min_frequency),
+            tf.math.log(embedding_max_frequency),
+            embedding_dims // 2,
+        )
+    )
+    angular_speeds = 2.0 * math.pi * frequencies
+    #angular_speeds = tf.cast(angular_speeds, tf.float16)
+    embeddings = tf.concat(
+        [tf.sin(angular_speeds * x), tf.cos(angular_speeds * x)], axis=3
+    )
+    return embeddings
+
+def ResidualBlock(width):
+    def apply(x):
+        input_width = x.shape[3]
+        if input_width == width:
+            residual = x
+        else:
+            residual = layers.Conv2D(width, kernel_size=1)(x)
+        x = layers.BatchNormalization(center=False, scale=False)(x)
+        x = layers.Conv2D(
+            width, kernel_size=3, padding="same", activation=keras.activations.swish
+        )(x)
+        x = layers.Conv2D(width, kernel_size=3, padding="same")(x)
+        x = layers.Add()([x, residual])
+        return x
+
+    return apply
+
+def DownBlock(width, block_depth):
+    def apply(x):
+        x, skips = x
+        for _ in range(block_depth):
+            x = ResidualBlock(width)(x)
+            skips.append(x)
+        x = layers.AveragePooling2D(pool_size=2)(x)
+        return x
+
+    return apply
+
+def UpBlock(width, block_depth):
+    def apply(x):
+        x, skips = x
+        x = layers.UpSampling2D(size=2, interpolation="bilinear")(x)
+        for _ in range(block_depth):
+            x = layers.Concatenate()([x, skips.pop()])
+            x = ResidualBlock(width)(x)
+        return x
+
+    return apply
+
 class DiffusionModel(keras.Model):
     def __init__(self, input_size, block_depth, widths):
         super().__init__()
