4626674a870da3c36bbbe10415a7dd5d4ad8396f
hayden-donnelly <hayden.git@proton.me>
2023-04-16 18:14:08 -0400

    Setup docker env.

--- utilities/tensorflow_to_onnx.py
+++ utilities/tensorflow_to_onnx.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import tf2onnx
 
 input_path = "../data/models/"
-model_name = "simple_upsampler"
+model_name = "simple_upsampler_bilinear"
 file_type = ''
 output_path = "../data/models/onnx_models/"
 
@@ -10,4 +10,4 @@ output_path = "../data/models/onnx_models/"
 pre_model = tf.keras.models.load_model(input_path + model_name + file_type)
 
 # Convert h5 to onnx.
-tf2onnx.convert.from_keras(pre_model, output_path = output_path + model_name + ".onnx", opset = 11)
+tf2onnx.convert.from_keras(pre_model, output_path = output_path + model_name + ".onnx", opset = 9)
