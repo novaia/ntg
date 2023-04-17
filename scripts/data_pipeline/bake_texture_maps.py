1526068531127fd6c9f7c03899f766efccca30c3
hayden-donnelly <hayden.git@proton.me>
2023-04-17 10:03:26 -0400

    Added script to automatically open blend and execute baking.

--- scripts/data_pipeline/bake_texture_maps.py
+++ scripts/data_pipeline/bake_texture_maps.py
@@ -1,44 +1,25 @@
+"""
+Opens a .blend file and executes code to bake texture maps from procedural material.
+"""
+
 import bpy
 import os
 
-# Get the active object
-obj = bpy.context.active_object
+# Define the path of the blend file to open
+blend_file = os.path.join(os.path.dirname(bpy.data.filepath), 'example.blend')
 
-# Get the material of the object
-mat = obj.data.materials[0]
+# Define the path of the script to execute
+script_file = os.path.join(os.path.dirname(bpy.data.filepath), 'bake_export.py')
 
-# Enable nodes for the material
-mat.use_nodes = True
+# Define a function that executes the script after opening the blend file
+def execute_script(scene):
+    # Unregister the handler
+    bpy.app.handlers.load_post.remove(execute_script)
+    # Execute the script
+    exec(compile(open(script_file).read(), script_file, 'exec'))
 
-# Create a texture node and link it to the material output
-tex_node = mat.node_tree.nodes.new('ShaderNodeTexImage')
-mat_output = mat.node_tree.nodes['Material Output']
-mat.node_tree.links.new(tex_node.outputs['Color'], mat_output.inputs['Surface'])
+# Register the function as a persistent handler
+bpy.app.handlers.load_post.append(execute_script)
 
-# Create a new image with the desired resolution and assign it to the texture node
-img = bpy.data.images.new(name=obj.name + '_Diffuse', width=4096, height=4096)
-tex_node.image = img
-
-# Select the texture node
-tex_node.select = True
-mat.node_tree.nodes.active = tex_node
-
-# Bake the diffuse map
-bpy.ops.object.bake(type='DIFFUSE', margin=16)
-
-# Save the image to a file in the same folder as the blend file
-img.filepath_raw = os.path.join(os.path.dirname(bpy.data.filepath), obj.name + '_Diffuse.png')
-img.file_format = 'PNG'
-img.save()
-
-# Create a new image with the desired resolution and assign it to the texture node
-img = bpy.data.images.new(name=obj.name + '_Normal', width=4096, height=4096)
-tex_node.image = img
-
-# Bake the normal map
-bpy.ops.object.bake(type='NORMAL', margin=16)
-
-# Save the image to a file in the same folder as the blend file
-img.filepath_raw = os.path.join(os.path.dirname(bpy.data.filepath), obj.name + '_Normal.png')
-img.file_format = 'PNG'
-img.save()
+# Open the blend file
+bpy.ops.wm.open_mainfile(filepath=blend_file)
