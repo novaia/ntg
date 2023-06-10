da82da82d66611d94cc7375ebd8dfce90feb23a5
hayden-donnelly <donnellyhd@outlook.com>
2023-06-09 20:02:21 -0400

    Wrapped in main checker.

--- utilities/generate_model_progress_pictures.py
+++ utilities/generate_model_progress_pictures.py
@@ -8,10 +8,11 @@ model_path = '../data/models/diffusion_models/'
 model_name = 'diffusion1'
 image_save_path = '../data/images/'
 
-for epoch in range(starting_epoch, ending_epoch):
-    model = models.load_model(model_path + model_name + '_epoch_' + str(epoch))
+if __name__ == '__main__':
+    for epoch in range(starting_epoch, ending_epoch):
+        model = models.load_model(model_path + model_name + '_epoch_' + str(epoch))
 
-    initial_noise = tf.random.normal(shape=(7, 256, 256, 1))
+        initial_noise = tf.random.normal(shape=(7, 256, 256, 1))
 
-    generated_images = reverse_diffusion(model, num_images = 7, diffusion_steps = 20, initial_noise = initial_noise)
-    save_images(generated_images, rows = 1, columns = 7, epoch = epoch)
+        generated_images = reverse_diffusion(model, num_images = 7, diffusion_steps = 20, initial_noise = initial_noise)
+        save_images(generated_images, rows = 1, columns = 7, epoch = epoch)
