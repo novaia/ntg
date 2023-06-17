fffc75a61833e69c6d1f1f4df1ee3e28a9612e4d
hayden-donnelly <donnellyhd@outlook.com>
2023-06-16 23:25:37 -0400

    Still debugging.

--- utilities/fid.py
+++ utilities/fid.py
@@ -20,136 +20,12 @@ import scipy
 from PIL import Image
 import intest
 
-def compute_statistics(
-    path, 
-    params, 
-    apply_fn, 
-    preprocessing_fn,
-    image_size,
-    img_size=None, 
-    batch_size=1 
-):
-    #idg = ImageDataGenerator(preprocessing_function = preprocessing_fn)
-    #image_iterator = idg.flow_from_directory(
-    #    path, 
-    #    target_size = image_size, 
-    #    batch_size = batch_size,
-    #    color_mode = 'rgb',
-    #    classes = ['']
-    #)
-    #
-    #activations = []
-    #for _ in tqdm(range(len(image_iterator))):
-    #    x = jnp.asarray(image_iterator.next()[0])
-    #    x = 2 * x - 1
-    #    predictions = apply_fn(params, jax.lax.stop_gradient(x))
-    #    activations.append(predictions.squeeze(axis=1).squeeze(axis=1))
-    #activations = jnp.concatenate(activations, axis=0)
-
-    images = []
-    img_size = (256, 256)
-    for f in tqdm(os.listdir(path)):
-        img = Image.open(os.path.join(path, f))
-        # convert if only a single channel
-        if img.mode == "L":
-            img = img.convert("RGB")
-        # resize if not the right size
-        if img_size is not None and img.size[:2] != img_size:
-            img = img.resize(
-                size=(img_size[0], img_size[1]),
-                resample=Image.BILINEAR,
-            )
-        img = np.array(img) / 255.0
-        images.append(img)
-
-    num_batches = int(len(images) // batch_size)
-    activations = []
-    for i in tqdm(range(num_batches)):
-        x = images[i * batch_size : i * batch_size + batch_size]
-        x = np.asarray(x)
-        x = 2 * x - 1
-        pred = apply_fn(params, jax.lax.stop_gradient(x))
-        activations.append(pred.squeeze(axis=1).squeeze(axis=1))
-    activations = jnp.concatenate(activations, axis=0)
-
-    mu = jnp.mean(activations, axis=0)
-    sigma = np.cov(activations, rowvar=False)
-
-    print('mu shape', mu.shape)
-    print('sigma shape', sigma.shape)
-
-    return mu, sigma
-
 def load_statistics(path):
     stats = np.load(path)
     mu, sigma = stats["mu"], stats["sigma"]
     return mu, sigma
 
-# Tries to load stastics from a file, if it fails then it computes them.
-def get_fid_statistics(
-    statistics_path, 
-    dataset_path, 
-    params, 
-    apply_fn, 
-    preprocessing_fn,
-    image_size,
-    save_statistics=False
-):
-    if statistics_path is not None and os.path.isfile(statistics_path):
-        mu, sigma = load_statistics(statistics_path)
-        print('FID statistics loaded from:', statistics_path)
-        return mu, sigma
-    else:
-        print('FID statistics not found, computing...')
-        mu, sigma = compute_statistics(
-            dataset_path, 
-            params, 
-            apply_fn, 
-            preprocessing_fn, 
-            image_size
-        )
-        
-        if save_statistics:
-            np.savez(statistics_path, mu=mu, sigma=sigma)
-            print('FID statistics saved to:', statistics_path)
-        
-        return mu, sigma
-
-def compute_frechet_distance(mu1, mu2, sigma1, sigma2, eps=1e-6):
-    mu1 = np.atleast_1d(mu1)
-    mu2 = np.atleast_1d(mu2)
-    sigma1 = np.atleast_1d(sigma1)
-    sigma2 = np.atleast_1d(sigma2)
-    
-    assert mu1.shape == mu2.shape
-    assert sigma1.shape == sigma2.shape
-
-    diff = mu1 - mu2
-
-    covmean, _ = scipy.linalg.sqrtm(sigma1.dot(sigma2), disp=False)
-    if not np.isfinite(covmean).all():
-        msg = (
-            "fid calculation produces singular product; "
-            "adding %s to diagonal of cov estimates"
-        ) % eps
-        print(msg)
-        offset = np.eye(sigma1.shape[0]) * eps
-        covmean = scipy.linalg.sqrtm(
-            (sigma1 + offset).dot(sigma2 + offset), 
-            disp=False
-        )[0]
-
-    # Numerical error might give slight imaginary component.
-    if np.iscomplexobj(covmean):
-        if not np.allclose(np.diagonal(covmean).imag, 0, atol=1e-3):
-            m = np.max(np.abs(covmean.imag))
-            raise ValueError("Imaginary component {}".format(m))
-        covmean = covmean.real
-
-    tr_covmean = np.trace(covmean)
-    return diff.dot(diff) + np.trace(sigma1) + np.trace(sigma2) - 2 * tr_covmean
-
-def compute_frechet_distance_alt(mu1, mu2, sigma1, sigma2, eps=1e-6):
+def test_function(mu1, mu2, sigma1, sigma2, eps=1e-6):
     # Taken from: https://github.com/mseitzer/pytorch-fid/blob/master/src/pytorch_fid/fid_score.py
     mu1 = np.atleast_1d(mu1)
     mu2 = np.atleast_1d(mu2)
@@ -186,35 +62,8 @@ def preprocessing_function(image):
     return image
 
 if __name__ == '__main__':
-    statistics_path = '../data/dataset_info/second_pass_fid_stats.npz'
-    dataset_path = '../../fid_test1/'
-    second_dataset_path = '../../fid_test2/'
+    mu1, sigma1 = load_statistics('../data/dataset_info/fid_test1_stats.npz')
+    mu2, sigma2 = load_statistics('../data/dataset_info/fid_test2_stats.npz')
 
-    rng = jax.random.PRNGKey(0)
-    #model = fid_inception.InceptionV3()
-    model = intest.InceptionV6(pretrained=True)
-    params = model.init(rng, jnp.ones((1, 256, 256, 3)))
-    apply_fn = jax.jit(functools.partial(model.apply, train=False))
-
-    mu1, sigma1 = get_fid_statistics(
-        statistics_path,
-        dataset_path,
-        params,
-        apply_fn,
-        preprocessing_function,
-        image_size=(256, 256),
-        save_statistics=True
-    )
-
-    mu2, sigma2 = get_fid_statistics(
-        'none',
-        second_dataset_path,
-        params,
-        apply_fn,
-        preprocessing_function,
-        image_size=(256, 256),
-        save_statistics=False
-    )
-
-    fid = compute_frechet_distance_alt(mu1, mu2, sigma1, sigma2)
+    fid = test_function(mu1, mu2, sigma1, sigma2)
     print('FID:', fid)
