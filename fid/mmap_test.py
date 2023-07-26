451140b92dee1a131894696b6e28a8895758d88c
hayden-donnelly <hayden.git@proton.me>
2023-07-26 19:17:33 -0400

    Debugging mmap fid.

--- fid/mmap_test.py
+++ fid/mmap_test.py
@@ -5,19 +5,20 @@ import mmap
 filename = 'mmap_file'
 dtype = 'float32'
 
-f = open(filename, 'w+b')
 num_items = 50
 dtype_size = np.dtype(dtype).itemsize
 file_size = dtype_size * num_items
-f.write(b"\0" * file_size)
 print(f'Create file of size: {file_size}')
 
-with open(filename, "r+b") as f:
+with open(filename, "w+b") as f:
+    f.write(b"\0" * file_size)
+    f.flush()
     mm = mmap.mmap(f.fileno(), file_size)
 
     for i in range(num_items):
         mm[i * dtype_size : (i + 1) * dtype_size] = np.array([float(i)], dtype=dtype).tobytes()
     
     for i in range(num_items):
+        break
         arr = np.frombuffer(mm[i * dtype_size : (i + 1) * dtype_size], dtype=dtype)
         print(arr[0])
