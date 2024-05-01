d87391cc667d996cdab65ddcc2c7a750cc9cdc4d
hayden-donnelly <donnellyhd@outlook.com>
2024-04-30 22:32:54 -0400

    read_tiff.py can now convert an entire directory of tiffs to pngs.

--- data_pipeline/read_tiff.py
+++ data_pipeline/read_tiff.py
@@ -1,30 +1,31 @@
+import os, argparse
 from tifffile import imread, TiffFile
 from PIL import Image
 import zarr
 import numpy as np
 
-tif = TiffFile('data/example3.tif')
-store = tif.aszarr(key=0)
-print(store)
-z = zarr.open(store, mode="r")
-print(z)
-heightmap = np.array(z, np.float32)
-heightmap = np.clip(heightmap, 0.0, heightmap.max())
-print(heightmap.max())
-print(heightmap.min())
-heightmap = (heightmap - heightmap.min()) / (heightmap.max() - heightmap.min())
-print(heightmap.mean())
-print(heightmap)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_path', type=str, required=True)
+    parser.add_argument('--output_path', type=str, required=True)
+    args = parser.parse_args()
 
-heightmap = np.array(heightmap * 255, np.uint8)
+    if not os.path.exists(args.output_path):
+        os.makedirs(args.output_path)
 
-heightmap_image = Image.fromarray(heightmap)
-heightmap_image.save('data/heightmap3.png')
+    input_paths = os.listdir(args.input_path)
+    for path in input_paths:
+        tif = TiffFile(os.path.join(args.input_path, path))
+        store = tif.aszarr(key=0)
+        z = zarr.open(store, mode='r')
+        heightmap = np.array(z, np.float32)
+        heightmap = np.clip(heightmap, 0.0, heightmap.max())
+        heightmap = heightmap / heightmap.max()
+        heightmap = np.array(heightmap * 255, np.uint8)
+        heightmap_image = Image.fromarray(heightmap)
+        heightmap_image.save(os.path.join(args.output_path, f'{path[:-3]}png'))
+        store.close()
+        heightmap_image.close()
 
-store.close()
-
-#exit()
-with TiffFile('data/example.tif') as tif:
-    for page in tif.pages:
-        for tag in page.tags:
-            print(tag.name, tag.value)
+if __name__ == '__main__':
+    main()
